d8f1d312ef47c0992384d4b9d749624a2b03fa3e
Yukari-Tamura <midorinakano125@gmail.com>
2022-09-04 19:08:20 -0500

    feels

--- src/controllers/productsControllers.ts
+++ src/controllers/productsControllers.ts
@@ -5,104 +5,177 @@ class productsController {
 
     //Listar productos
     public async list(req:Request, res:Response):Promise <void>{
-        const products = await db.query('SELECT * FROM products');
-        res.json(products)
+
+        try {
+            const products = await db.query('SELECT * FROM products');
+            res.json(products)
+        }catch(error){
+            console.log('error ', error);
+        }
+
+        
     }
 
     //Listar un solo elelemento
     public async listId(req:Request, res:Response):Promise <any> {
-        console.log("parametros por id",req.params)
-        const {id} = req.params;
-        const product = await db.query('SELECT * FROM products WHERE id = ?',[id])
-        if(product.length > 0){
-            return res.json(product[0])
-            
+        try {
+            console.log("parametros por id",req.params)
+            const {id} = req.params;
+            const product = await db.query('SELECT * FROM products WHERE id = ?',[id])
+            if(product.length > 0){
+                return res.json(product[0])
+                
+            }
+            res.status(404).json({text: "The product don't found id"});
+        }catch(error){
+            console.log('error ', error);
         }
-        res.status(404).json({text: "The product don't found id"});
+
+        
     }
 
     //Limiar la busqueda
     public async limit(req:Request, res:Response):Promise <any> {
-        const {init} = req.params;
-        const {limite} = req.params;
-        const number = parseInt(limite);
-        const initial = parseInt(init);
-        const products = await db.query('SELECT * FROM products LIMIT ?,?',[initial,number])
-        res.json(products);
+        try {
+            const {init} = req.params;
+            const {limite} = req.params;
+            const number = parseInt(limite);
+            const initial = parseInt(init);
+            const products = await db.query('SELECT * FROM products LIMIT ?,?',[initial,number])
+            res.json(products);
+        }catch(error){
+            console.log('error ', error);
+        }
+
+        
     }
 
     //Verificar si existe el item
     public async verifyItem(req:Request, res:Response):Promise<any> {
-        const {name} = req.params;
-        const item = await db.query('SELECT * FROM products WHERE name = ?',[name])
-        if(item.length > 0){
-            return res.json(item[0])
-        }else {
-            return res.json([])
-        }   
+        try {
+            const {name} = req.params;
+            const item = await db.query('SELECT * FROM products WHERE name = ?',[name])
+            if(item.length > 0){
+                return res.json(item[0])
+            }else {
+                return res.json([])
+            } 
+        }catch(error){
+            console.log('error ', error);
+        }
+
+          
     }
 
     //filtro de informacion para cargar galeria de top 20
     public async filterData(req:Request, res:Response):Promise<any> {
-        const {filtro} = req.params;
-        const product = await db.query('SELECT * FROM products WHERE name = ?',[filtro])
-        res.json(product)
+        try {
+            const {filtro} = req.params;
+            const product = await db.query('SELECT * FROM products WHERE name = ?',[filtro])
+            res.json(product)
+        }catch(error){
+            console.log('error ', error);
+        }
+
+        
     }
 
     //filtrador de datos por url Category
     public async filterUrlCategory(req:Request, res:Response):Promise<any> {
-        const {data} = req.params;
-        const {url} = req.params;
-        const product = await db.query('SELECT * FROM products WHERE  '+data+' = ?',[url])
-        res.json(product)
+        try {
+            const {data} = req.params;
+            const {url} = req.params;
+            const product = await db.query('SELECT * FROM products WHERE  '+data+' = ?',[url])
+            res.json(product)
+        }catch(error){
+            console.log('error ', error);
+        }
+
+        
     }
 
     //filtrar por url
     public async filterUrlItem(req:Request, res:Response):Promise<any> {
-        const {url} = req.params;
-        const item = await db.query('SELECT * FROM products WHERE url=?', [url]);
-        res.json(item)
+        try {
+            const {url} = req.params;
+            const item = await db.query('SELECT * FROM products WHERE url=?', [url]);
+            res.json(item)
+        }catch(error){
+            console.log('error ', error);
+        }
+
+        
 
     }
 
     //filtrar por data y campos
     public async filterMultipleData(req:Request, res:Response):Promise<any> {
-        const {item} = req.params;
-        const {valor} = req.params;
-        const products = await db.query('SELECT * FROM products WHERE '+item+' = ?', [valor])
-        res.json(products);
+        try {
+            const {item} = req.params;
+            const {valor} = req.params;
+            const products = await db.query('SELECT * FROM products WHERE '+item+' = ?', [valor])
+            res.json(products);
+        }catch(error){
+            console.log('error ', error);
+        }
+
+        
     }
 
     //filtrador con limitadores
     public async filterLimitData(req:Request, res:Response):Promise<any> {
-        const {item} = req.params;
-        const {valor} = req.params;
-        const {init} = req.params;
-        const iniciador = parseInt(init);
-        const {limit} = req.params;
-        const limitador = parseInt(limit);
-        const products = await db.query('SELECT * FROM products WHERE '+item+' = ? LIMIT ?,?',[valor,iniciador, limitador])
-        res.json(products)
+        try {
+            const {item} = req.params;
+            const {valor} = req.params;
+            const {init} = req.params;
+            const iniciador = parseInt(init);
+            const {limit} = req.params;
+            const limitador = parseInt(limit);
+            const products = await db.query('SELECT * FROM products WHERE '+item+' = ? LIMIT ?,?',[valor,iniciador, limitador])
+            res.json(products)
+        }catch(error){
+            console.log('error ', error);
+        }
+
+       
     }
 
     //Crear
     public async create(req:Request, res:Response):Promise<void>{
-        await db.query('INSERT INTO products SET ? ', [req.body]);
-        res.json({message: 'Save Product'})
+        try {
+            await db.query('INSERT INTO products SET ? ', [req.body]);
+            res.json({message: 'Save Product'})
+        }catch(error){
+            console.log('error ', error);
+        }
+
+        
     }
 
     //Eliminar
     public async delete(req:Request, res:Response):Promise<any> {
-        const {id} = req.params;
-        await db.query('DELETE FROM products WHERE id=?', [id]);
-        res.json({message: 'Product Delete'})
+        try {
+            const {id} = req.params;
+            await db.query('DELETE FROM products WHERE id=?', [id]);
+            res.json({message: 'Product Delete'})
+        }catch(error){
+            console.log('error ', error);
+        }
+
+        
     }
 
     //Actualizar
     public async update(req:Request, res:Response):Promise<void>{
-        const {id} = req.params;
-        await db.query('UPDATE products set ? WHERE id=?', [req.body, id]);
-        res.json({message: 'The product update'})
+        try {
+            const {id} = req.params;
+            await db.query('UPDATE products set ? WHERE id=?', [req.body, id]);
+            res.json({message: 'The product update'})
+        }catch(error){
+            console.log('error ', error);
+        }
+
+        
     }
     
 }
--- src/controllers/subcategoriesControllers.ts
+++ src/controllers/subcategoriesControllers.ts
@@ -4,70 +4,115 @@ import db from "../database";
 class SubcategoriesController {
     //listar
     public async list(req:Request, res:Response):Promise <void> {
-        const subcategories = await db.query('SELECT * FROM subcategories');
-        res.json(subcategories);
+        try {
+            const subcategories = await db.query('SELECT * FROM subcategories');
+            res.json(subcategories);
+        }catch(error){
+            console.log('error ', error);
+        }
+        
     }
 
     //Listar un solo elemento
     public async listId(req:Request, res:Response):Promise <any> {
-        const {id} = req.params;
-        const subcategory = await db.query('SELECT * FROM subcategories WHERE id = ?',[id]);
-        if(subcategory.length > 0){
-            return res.json(subcategory[0]);
+        try {
+            const {id} = req.params;
+            const subcategory = await db.query('SELECT * FROM subcategories WHERE id = ?',[id]);
+            if(subcategory.length > 0){
+                return res.json(subcategory[0]);
+            }
+            res.status(404).json({text: "The subcategory don't found"})
+        }catch(error){
+            console.log('error ', error);
         }
-        res.status(404).json({text: "The subcategory don't found"})
+       
     }
 
     //Filtrador de datos
     public async filterItem(req:Request, res:Response):Promise<any> {
-        const {name} = req.params;
-        const item = await db.query('SELECT * FROM subcategories WHERE name = ?',[name])
-        if(item.length > 0){
-            return res.json(item[0])
-        }else {
-            return res.json({text: "The item don't found"})
+        try {
+            const {name} = req.params;
+            const item = await db.query('SELECT * FROM subcategories WHERE name = ?',[name])
+            if(item.length > 0){
+                return res.json(item[0])
+            }else {
+                return res.json({text: "The item don't found"})
+            }
+        }catch(error){
+            console.log('error ', error);
         }
+        
     }
 
     //filtrador por titulos de categoria
     public async filterTitleCategory(req:Request, res:Response):Promise<any> {
-        const {title} = req.params;
-        const item = await db.query('SELECT * FROM subcategories WHERE title_list = ?',[title])
-        res.json(item);
+        try {
+            const {title} = req.params;
+            const item = await db.query('SELECT * FROM subcategories WHERE title_list = ?',[title])
+            res.json(item);
+        }catch(error){
+            console.log('error ', error);
+        }
+        
     }
 
     //filter categories
     public async filterCategory(req:Request, res:Response):Promise<any> {
-        const {idCategory} = req.params;
-        const item = await db.query('SELECT * FROM subcategories WHERE category = ?',[idCategory])
-        res.json(item);
+        try {
+            const {idCategory} = req.params;
+            const item = await db.query('SELECT * FROM subcategories WHERE category = ?',[idCategory])
+            res.json(item);
+        }catch(error){
+            console.log('error ', error);
+        }
+        
     }
 
     //filtrador por url
     public async filterUrl(req:Request, res:Response):Promise<any>{
-        const {url} = req.params;
-        const item = await db.query('SELECT * FROM subcategories WHERE url = ?', [url])
-        res.json(item)
+        try {
+            const {url} = req.params;
+            const item = await db.query('SELECT * FROM subcategories WHERE url = ?', [url])
+            res.json(item)
+        }catch(error){
+            console.log('error ', error);
+        }
+        
     }
 
     //Crear
     public async create(req:Request, res:Response):Promise<void> {
-        await db.query('INSERT INTO subcategories SET ?', [req.body]);
-        res.json({message: 'Save Subcategory'})
+        try {
+            await db.query('INSERT INTO subcategories SET ?', [req.body]);
+            res.json({message: 'Save Subcategory'})
+        }catch(error){
+            console.log('error ', error);
+        }
+        
     }
 
     //Elimiar
     public async delete(req:Request, res:Response):Promise<any> {
-        const {id} = req.params;
-        await db.query('DELETE FROM subcategories WHERE id=?', [id]);
-        res.json({message: 'Subcategory Delete'})
+        try {
+            const {id} = req.params;
+            await db.query('DELETE FROM subcategories WHERE id=?', [id]);
+            res.json({message: 'Subcategory Delete'})
+        }catch(error){
+            console.log('error ', error);
+        }
+        
     }
 
     //Actualizar
     public async update(req:Request, res:Response):Promise<void> {
-        const {id} = req.params;
-        await db.query('UPDATE subcategories set ? WHERE id=?', [req.body, id]);
-        res.json({message: 'The subcategory update'});
+        try {
+            const {id} = req.params;
+            await db.query('UPDATE subcategories set ? WHERE id=?', [req.body, id]);
+            res.json({message: 'The subcategory update'});
+        }catch(error){
+            console.log('error ', error);
+        }
+        
     }
 
 }
